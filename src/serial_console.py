e46c0a328724644ae8259f7ab6397c7485463deb
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2023-08-28 17:01:49 +0300

    further simplification of serial_console.py

--- src/serial_console.py
+++ src/serial_console.py
@@ -2,31 +2,28 @@ import customtkinter
 from PIL import Image
 
 import os
+import time
 
 import serial
 import serial.tools.list_ports as list_ports
 
 
 class SerialConsole(customtkinter.CTkFrame):
-    def __init__(self, master, serial_port=None, baudrate=None, line_ending=None):
+    def __init__(self, master, ports=None, serial_port=None, baudrate=None, line_ending=None):
         super().__init__(master)
 
         self.master = master
         self.serial_port = serial_port
         self.baudrate = baudrate
         self.line_ending = line_ending
-        
-        self.DATABITS = (serial.FIVEBITS, serial.SIXBITS, serial.SEVENBITS, serial.EIGHTBITS)
-        self.PARITY = (serial.PARITY_EVEN, serial.PARITY_ODD, serial.PARITY_NONE, serial.PARITY_MARK, serial.PARITY_SPACE)
-        self.PARITY_VAL = ('Even', 'Odd', 'None', 'Mark', 'Space')
-        self.STOPBITS = (serial.STOPBITS_ONE, serial.STOPBITS_ONE_POINT_FIVE, serial.STOPBITS_TWO)
-        self.ports = {p.name: p.description for p in list_ports.comports()}
-        self.currentPort = serial.Serial(port=None, baudrate=115200, timeout=0, write_timeout=0)
+
+        self.currentPort = serial.Serial(
+            port=None, baudrate=115200, timeout=0, write_timeout=0)
         self.sent_texts = []
         self.sent_texts_index = 0
         self.isEndByNL = True
         self.lastUpdatedBy = 2
-     
+
         icons_folder = os.path.join(os.getcwd(), 'src', 'icons')
         send_button_icon = customtkinter.CTkImage(light_image=Image.open(os.path.join(icons_folder, "send_button_light.png")),
                                                   dark_image=Image.open(os.path.join(
@@ -43,12 +40,12 @@ class SerialConsole(customtkinter.CTkFrame):
         self.grid_columnconfigure((1, 2), weight=0)
 
         self.rx_textbox = customtkinter.CTkTextbox(
-            self, bg_color='#343638', state="disabled", wrap="word", border_width=5, corner_radius=0)
+            self, bg_color='#343638', state="disabled", wrap="word", border_width=2, corner_radius=0)
         self.rx_textbox.grid(row=0, column=0, columnspan=3,
                              padx=5, pady=5, sticky="nsew")
-        self.rx_textbox.insert('0.0', "RX: \n")
 
-        self.tx_entrybox = customtkinter.CTkEntry(self)
+        self.tx_entrybox = customtkinter.CTkEntry(
+            self, border_width=2, corner_radius=0)
         self.tx_entrybox.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
 
         self.clear_button = customtkinter.CTkButton(
@@ -58,15 +55,33 @@ class SerialConsole(customtkinter.CTkFrame):
         self.send_button = customtkinter.CTkButton(
             self, text="", command=self.send, width=5, height=5, image=send_button_icon)
         self.send_button.grid(row=1, column=2, padx=5, pady=5)
-        
-        
+
+    def updateSerialSettings(self, serial_port=None, baudrate=None, line_ending=None):
+        self.serial_port = serial_port
+        self.baudrate = baudrate
+        self.line_ending = line_ending
 
     def clear(self):
         self.rx_textbox.configure(state="normal")
         self.rx_textbox.delete("0.0", 'end')
         self.rx_textbox.configure(state="disabled")
-        
-    def decode_esc(self, str_of_chr):
+
+    def getStrOfChr(self, chr_in_byte):
+        cd = ord(chr_in_byte)
+        if 0x20 <= cd and cd <= 0x7e or 0xa1 <= cd:
+            if cd == 92:
+                return '\\\\'
+            return chr(cd)
+        else:
+            if cd == 9:
+                return '\t'
+            elif cd == 10:
+                return '\n'
+            elif cd == 13:
+                return '\\r'
+        return '\\x{:02x}'.format(cd)
+
+    def decodeEsc(self, str_of_chr):
         sbs = bytes([ord(c) for c in str_of_chr])
         dbs = b''
         err = None
@@ -95,7 +110,8 @@ class SerialConsole(customtkinter.CTkFrame):
                 elif by == b'r':
                     dbs += b'\r'
                 elif by == b'x':
-                    err = {'from': idx-1, 'to': idx+3, 'msg': f'Value Error: invalid {str_of_chr[idx-1:idx+3]} escape at position {idx-1}'}
+                    err = {'from': idx-1, 'to': idx+3,
+                           'msg': f'Value Error: invalid {str_of_chr[idx-1:idx+3]} escape at position {idx-1}'}
                     break
                 else:
                     if by:
@@ -104,7 +120,8 @@ class SerialConsole(customtkinter.CTkFrame):
                     else:
                         ch = ''
                         to = idx
-                    err = {'from': idx-1, 'to': to, 'msg': f"Syntax Error: invalid escape sequence '\\{ch}' at position {idx-1}"}
+                    err = {'from': idx-1, 'to': to,
+                           'msg': f"Syntax Error: invalid escape sequence '\\{ch}' at position {idx-1}"}
                     break
             else:
                 dbs += by
@@ -115,7 +132,7 @@ class SerialConsole(customtkinter.CTkFrame):
         tx_text = str(self.tx_entrybox.get())
         lst = len(self.sent_texts)
         if tx_text != "":
-            bs, err = self.decode_esc(tx_text)
+            bs, err = self.decodeEsc(tx_text)
             if err:
                 self.writeConsole(err['msg'] + '\n', 2)
                 self.tx_entrybox.xview(err['from'])
@@ -124,7 +141,7 @@ class SerialConsole(customtkinter.CTkFrame):
                 return
             if lst > 0 and self.sent_texts[lst-1] != tx_text or lst == 0:
                 self.sent_texts.append(tx_text)
-            self.sent_textsPtr = len(self.sent_texts)
+            self.sent_texts_index = len(self.sent_texts)
             if self.line_ending == 1:
                 bs += b'\n'
             elif self.line_ending == 2:
@@ -132,15 +149,101 @@ class SerialConsole(customtkinter.CTkFrame):
             elif self.line_ending == 3:
                 bs += b'\r\n'
             self.currentPort.write(bs)
-            tx_text = ''.join([self.get_str_of_chr(bytes([i])) for i in bs])
+            tx_text = ''.join([self.getStrOfChr(bytes([i])) for i in bs])
             self.writeConsole(tx_text)
-            
-    def writeConsole(self, text, upd=0):
-            self.rx_textbox.configure(state="normal")
-            self.rx_textbox.insert('end', text + "\n")
-            self.rx_textbox.see('end')
-            self.rx_textbox.configure(state="disabled")
             self.tx_entrybox.delete(0, 'end')
+
+    def upKeyCmd(self, event):
+        if self.sent_texts_index == len(self.sent_texts):
+            self.lastTxText = str(self.tx_entrybox.get())
+        if self.sent_texts_index > 0:
+            self.sent_texts_index -= 1
+            self.tx_entrybox.delete(0, "end")
+            self.tx_entrybox.insert(
+                "end", self.sent_texts[self.sent_texts_index])
+
+    def downKeyCmd(self, event):
+        if self.sent_texts_index < len(self.sent_texts):
+            self.sent_texts_index += 1
+            self.tx_entrybox.delete(0, "end")
+            if self.sent_texts_index == len(self.sent_texts):
+                self.tx_entrybox.insert("end", self.lastTxText)
+            else:
+                self.tx_entrybox.insert(
+                    "end", self.sent_texts[self.sent_texts_index])
+
+    def changePort(self, event):
+        if self.serial_port == self.currentPort.port:
+            return
+        self.disableSending()
+        if self.currentPort.is_open:
+            self.currentPort.close()
+            self.writeConsole('Serial port closed.\n', 2)
+        self.currentPort.port = self.serial_port
+        self.writeConsole('Connecting...', 2)
+        self.update()
+        try:
+            self.currentPort.open()
+        except:
+            # portCbo.set('Select port')
+            self.writeConsole('failed!!!\n', 2)
+            self.currentPort.port = None
+        if self.currentPort.is_open:
+            self.enableSending()
+            self.rxPolling()
+            self.writeConsole('done.\n', 2)
+
+    def changeBaudrate(self, event):
+        self.currentPort.baudrate = self.baudrate
+
+    def writeConsole(self, text, upd=0):
+        ad = ''
+        if not upd:
+            if not self.lastUpdatedBy and self.isEndByNL or self.lastUpdatedBy:
+                ad += 'RX'
+                if ad:
+                    ad += ' >> '
+                if not self.isEndByNL:
+                    ad = '\n' + ad
+        elif upd == 1:
+            if self.lastUpdatedBy == 1 and self.isEndByNL or self.lastUpdatedBy != 1:
+                ad = 'TX'
+                ad += ' >> '
+                if not self.isEndByNL:
+                    ad = '\n' + ad
+        elif upd == 2:
+            if self.lastUpdatedBy != 2:
+                ad = '\n'
+                if not self.isEndByNL:
+                    ad += '\n'
+        else:
+            return
+        if upd != 2 and self.lastUpdatedBy == 2:
+            ad = '\n' + ad
+        ad += text
+        self.rx_textbox.configure(state="normal")
+        self.rx_textbox.insert('end', ad)
+        self.rx_textbox.see('end')
+        self.rx_textbox.configure(state="disabled")
+        if text[-1] == '\n':
+            self.isEndByNL = True
+        else:
+            self.isEndByNL = False
+        self.lastUpdatedBy = upd
+
+    def rxPolling(self):
+        if not self.currentPort.is_open:
+            return
+        preset = time.perf_counter_ns()
+        try:
+            while self.currentPort.in_waiting > 0 and time.perf_counter_ns()-preset < 2000000:  # loop duration about 2ms
+                ch = self.currentPort.read()
+                txt = ''
+                txt += self.getStrOfChr(ch)
+                self.writeConsole(txt)
+        except serial.SerialException as se:
+            self.closePort()
+        self.after(10, self.rxPolling)  # polling in 10ms interval
 
     def disableSending(self):
         self.send_button.configure(state="disabled")
@@ -149,9 +252,10 @@ class SerialConsole(customtkinter.CTkFrame):
     def enableSending(self):
         self.send_button.configure(state="normal")
         self.tx_entrybox.bind('<Return>', self.send)
-        
-    def receive(self, rx_text):
-        self.rx_textbox.configure(state="normal")
-        self.rx_textbox.insert('end', rx_text)
-        self.rx_textbox.configure(state="disabled")
-        self.rx_textbox.see('end')
+
+    def closePort(self):
+        if self.currentPort.is_open:
+            self.currentPort.close()
+            self.writeConsole('Serial port closed.\n', 2)
+            self.currentPort.port = None
+            self.disableSending()
