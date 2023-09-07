15f3d711f6e7292cfebb84212d9c8861a5974e8e
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2023-09-07 17:48:05 +0300

    started raman search from scratch

--- src/camera_view.py
+++ src/camera_view.py
@@ -7,3 +7,6 @@ from pygrabber.dshow_graph import FilterGraph
 class CameraView(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
+        
+        self.label1 = customtkinter.CTkLabel(self, text="Camera View")
+        self.label1.pack(fill="both", expand=True)
