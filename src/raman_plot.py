0bc4499ff2d4added2fc8328b592b388e2cfa119
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2023-09-10 18:57:48 +0300

    initialized raman and span select plots

--- src/raman_plot.py
+++ src/raman_plot.py
@@ -34,5 +34,25 @@ class RamanPlot(customtkinter.CTkFrame):
         self.span_selector_frame.pack(
             fill="both", expand=False, padx=10, pady=10)
 
+        self.main_fig = Figure(figsize=(5, 4), dpi=100)
+        self.main_plot = self.main_fig.add_subplot(111)
+        self.main_plot.plot([1, 2, 3, 4, 5], [10, 20, 30, 40, 50])
+
+        main_canvas = FigureCanvasTkAgg(self.main_fig, master=self.plot_frame)
+        main_canvas.draw()
+
+        # main_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        toolbar = NavigationToolbar2Tk(main_canvas, self.plot_frame)
+        toolbar.update()
+        main_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        
+        self.span_fig = Figure(figsize=(5, 1), dpi=100)
+        self.span_plot = self.span_fig.add_subplot(111)
+        self.span_plot.plot([1, 2, 3, 4, 5], [10, 20, 30, 40, 50])
+        
+        span_canvas = FigureCanvasTkAgg(self.span_fig, master=self.span_selector_frame)
+        span_canvas.draw()
+        span_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
     def changeTheme(self, color_palette):
         pass
