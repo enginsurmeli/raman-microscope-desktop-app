bbf6bf34bce0277c27959b71a900268dd72b5529
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2023-09-10 19:37:15 +0300

    minor fix to plots

--- src/raman_plot.py
+++ src/raman_plot.py
@@ -23,10 +23,7 @@ class RamanPlot(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        # self.grid_rowconfigure(0, weight=1)
-        # self.grid_rowconfigure(1, weight=0)
-        # self.grid_columnconfigure(0, weight=1)
-
+        self.master = master
         self.plot_frame = customtkinter.CTkFrame(self)
         self.plot_frame.pack(fill="both", expand=True, padx=10, pady=10)
 
@@ -41,17 +38,22 @@ class RamanPlot(customtkinter.CTkFrame):
         main_canvas = FigureCanvasTkAgg(self.main_fig, master=self.plot_frame)
         main_canvas.draw()
         main_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        
+
         self.span_fig = Figure(figsize=(5, 1), dpi=100)
         self.span_plot = self.span_fig.add_subplot(111)
         self.span_plot.plot([1, 2, 3, 4, 5], [10, 20, 30, 40, 50])
-        
-        span_canvas = FigureCanvasTkAgg(self.span_fig, master=self.span_selector_frame)
+
+        span_canvas = FigureCanvasTkAgg(
+            self.span_fig, master=self.span_selector_frame)
         span_canvas.draw()
-        self.toolbar = NavigationToolbar2Tk(main_canvas, self.span_selector_frame)
-        unwanted_buttons = ["Subplots", "Save", "Back", "Forward", "Home", "Pan", "Zoom"]
+        self.toolbar = NavigationToolbar2Tk(
+            main_canvas, self.span_selector_frame)
+
+        unwanted_buttons = ["Subplots", "Save",
+                            "Back", "Forward", "Home", "Pan", "Zoom"]
         for button in unwanted_buttons:
             self.toolbar._buttons[str(button)].pack_forget()
+
         self.toolbar.update()
         span_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
@@ -65,7 +67,7 @@ class RamanPlot(customtkinter.CTkFrame):
         self.main_plot.spines['left'].set_color(color_palette[3])
         self.main_plot.spines['right'].set_color(color_palette[3])
         self.main_plot.set_yticks([])
-        
+
         self.span_fig.set_facecolor(color_palette[0])
         self.span_plot.set_facecolor(color_palette[0])
         self.span_plot.tick_params(axis='x', colors=color_palette[3])
@@ -76,9 +78,8 @@ class RamanPlot(customtkinter.CTkFrame):
         self.span_plot.spines['right'].set_color(color_palette[3])
         self.span_plot.set_yticks([])
         self.span_plot.set_xticks([])
-        
+
         self.toolbar.config(background=color_palette[0])
         self.toolbar.winfo_children()[-2].config(background=color_palette[0])
         self.toolbar._message_label.config(
             background=color_palette[0], foreground=color_palette[3])
-        self.toolbar.update()
