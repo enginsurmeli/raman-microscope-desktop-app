603e49fd43804cc74d162d2cbab72fb3f65e01f7
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2023-09-11 14:02:00 +0300

    minor changes to camera

--- src/camera_view.py
+++ src/camera_view.py
@@ -9,36 +9,37 @@ from PIL import Image, ImageTk
 class CameraView(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-        
+
         self.master = master
 
     def connect_camera(self, camera_index: int = 0):
         self.vid = VideoCapture(camera_index)
         self.canvas = tk.Canvas(self, highlightthickness=0)
         self.canvas.pack(fill=tk.BOTH, expand=True)
-        
+
         self.delay = 15
         self.update()
-        
+
     def update(self):
         # Get a frame from the video source
-        return_value, frame = self.vid.get_frame()        
+        return_value, frame = self.vid.get_frame()
 
         if return_value:
             try:
-                self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame))
-                self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
+                self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+                self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
             except BaseException:
-                    import sys
-                    print(sys.exc_info()[0])
-                    import traceback
-                    print(traceback.format_exc())                
+                import sys
+                print(sys.exc_info()[0])
+                import traceback
+                print(traceback.format_exc())
             finally:
-                pass    
+                pass
 
         self.after(self.delay, self.update)
-        
+
+
 class VideoCapture:
     def __init__(self, video_source=0):
         self.vid = cv2.VideoCapture(video_source)
