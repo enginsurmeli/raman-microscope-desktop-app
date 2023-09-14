399947da281b62453c1a1361644c29d53338ffc3
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2023-09-14 10:47:13 +0300

    fix treeview alternate row colors after sorting

--- src/raman_search.py
+++ src/raman_search.py
@@ -5,6 +5,8 @@ from tkinter import ttk
 import os
 import numpy as np
 
+import random
+
 
 class RamanSearch(customtkinter.CTkFrame):
     def __init__(self, master):
@@ -47,7 +49,7 @@ class RamanSearch(customtkinter.CTkFrame):
         scrollbar.grid(row=0, column=1, sticky="ns", pady=10)
 
         self.treeview.bind("<Double-1>", self.OnDoubleClick)
-        
+
         self.initializeTreeview()
 
     def ramanSearch(self):
@@ -66,6 +68,7 @@ class RamanSearch(customtkinter.CTkFrame):
         # rearrange items in sorted positions
         for index, (val, k) in enumerate(l):
             treeview.move(k, '', index)
+            treeview.item(k, tags=('oddrow', 'evenrow')[index % 2])
 
         # reverse sort next time
         treeview.heading(column, command=lambda: self.SortTreeviewColumn(
@@ -83,20 +86,19 @@ class RamanSearch(customtkinter.CTkFrame):
         treestyle.configure(
             "Treeview.Heading", background=color_palette[4], foreground=color_palette[3], borderwidth=0)
         treestyle.map('Treeview', background=[('selected', color_palette[1])],
-                           foreground=[('selected', color_palette[2])])
+                      foreground=[('selected', color_palette[2])])
         self.treeview.bind(
-            "<<Treeview>>", lambda event: self.treeview.focus_set()) # remove focus from treeview
-        
+            "<<Treeview>>", lambda event: self.treeview.focus_set())  # remove focus from treeview
+
     def initializeTreeview(self):
         # Initialize Raman Spectra Database
-            for subdir, dirs, files in os.walk(os.path.join(self.initialdir, self.raman_db_folder)):
-                self.db_filepath_list = []
-                for file in files:
-                    if file.endswith('.txt'):
-                        # self.treeview.insert(
-                        #     '', tk.END, values=('--', file.replace('.txt', '')))
-                        tags = ['oddrow', 'evenrow']
-                        self.treeview.insert(
-                            '', tk.END, values=(file.replace('.txt', ''), '--'), tags=tags[len(self.treeview.get_children()) % 2])
-                        # TODO: Use this list inside SearchRamanDB method.
-                        self.db_filepath_list.append(os.path.join(subdir, file))
+        for subdir, dirs, files in os.walk(os.path.join(self.initialdir, self.raman_db_folder)):
+            self.db_filepath_list = []
+            for file in files:
+                if file.endswith('.txt'):
+                    # self.treeview.insert(
+                    #     '', tk.END, values=('--', file.replace('.txt', '')))
+                    self.treeview.insert(
+                        '', tk.END, values=(file.replace('.txt', ''), random.randint(0, 100)), tags=('oddrow', 'evenrow')[len(self.treeview.get_children()) % 2])
+                    # TODO: Use this list inside SearchRamanDB method.
+                    self.db_filepath_list.append(os.path.join(subdir, file))
