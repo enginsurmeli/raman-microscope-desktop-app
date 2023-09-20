b296ec92b266372eb7b41bf595febfd62548e413
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2023-09-20 16:50:14 +0300

    minor fix to scan button label

--- src/raman_scan.py
+++ src/raman_scan.py
@@ -52,10 +52,10 @@ class RamanScan(customtkinter.CTkFrame):
         start_scan_icon = customtkinter.CTkImage(light_image=Image.open(os.path.join(icons_folder, "start_scan_light.png")),
                                                  dark_image=Image.open(os.path.join(
                                                      icons_folder, "start_scan_dark.png")),
-                                                 size=(button_size[0]*2, button_size[1]*3))
+                                                 size=(button_size[0]*1.5, button_size[1]*2.5))
 
         self.start_scan_button = customtkinter.CTkButton(
-            self, text="Start\nScan", anchor="s", command=self.startRamanScan, width=button_size[0], height=button_size[1], image=start_scan_icon)
+            self, text="Start\nScan", command=self.startRamanScan, width=button_size[0], height=button_size[1], image=start_scan_icon, compound="top")
         self.start_scan_button.grid(row=0, column=2, rowspan=3,
                                     padx=inner_frame_padding, pady=inner_frame_padding)
 
