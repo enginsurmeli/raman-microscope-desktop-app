cae152d3f96d736e04863e6ac1d0211c3c60b3d5
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2023-10-09 16:24:07 +0300

    update thorlabs spectrometer example

--- misc/thorlabs_ccs_raman_spectrometer_example.py
+++ misc/thorlabs_ccs_raman_spectrometer_example.py
@@ -9,8 +9,9 @@ import matplotlib.pyplot as plt
 from ctypes import *
 import numpy as np
 
-os.chdir(r"C:\Program Files\IVI Foundation\VISA\Win64\Bin")
-lib = cdll.LoadLibrary("TLCCS_64.dll")
+lib_folder = 'thorlabs_lib'
+dll_filepath = os.path.join(os.path.dirname(__file__), '..', lib_folder, 'TLCCS_64.dll')
+lib = cdll.LoadLibrary(dll_filepath)
 
 # documentation: C:\Program Files\IVI Foundation\VISA\Win64\TLCCS\Manual
 
@@ -34,36 +35,44 @@ ccs_handle = c_int(0)
 # product_id = "0x8081"
 # usb_port = "0"
 # address = f"USB{usb_port}::0x1313::{product_id}::{serial_number}::RAW"
-lib.tlccs_init(b"USB0::0x1313::0x8081::M00822009::RAW",
-               1, 1, byref(ccs_handle))
+connect_spectrometer = lib.tlccs_init(b"USB0::0x1313::0x8081::M00822009::RAW",
+                                      1, 1, byref(ccs_handle))
 
-# set integration time in  seconds, ranging from 1e-5 to 6e1
-integration_time = c_double(10.0e-3)
-lib.tlccs_setIntegrationTime(ccs_handle, integration_time)
+if connect_spectrometer == 0:
+    is_connected = True
+    print("Spectrometer connected")
+else:
+    is_connected = False
+    print("Spectrometer not connected")
 
+if is_connected:
 
-# start scan
-lib.tlccs_startScan(ccs_handle)
+    # set integration time in  seconds, ranging from 1e-5 to 6e1
+    integration_time = c_double(10.0e-3)
+    lib.tlccs_setIntegrationTime(ccs_handle, integration_time)
 
-wavelengths = (c_double*3648)()
+    # start scan
+    lib.tlccs_startScan(ccs_handle)
 
-lib.tlccs_getWavelengthData(ccs_handle, 0, byref(
-    wavelengths), c_void_p(None), c_void_p(None))
+    wavelengths = (c_double*3648)()
 
-# retrieve data
-data_array = (c_double*3648)()
-lib.tlccs_getScanData(ccs_handle, byref(data_array))
+    lib.tlccs_getWavelengthData(ccs_handle, 0, byref(
+        wavelengths), c_void_p(None), c_void_p(None))
 
-raman_shift_nm = np.ndarray(shape=(3648,), dtype=float, buffer=wavelengths)
-raman_shift_inverse_cm = 1e7*(1/532 - 1/raman_shift_nm)
-intensity = np.ndarray(shape=(3648,), dtype=float, buffer=data_array)
+    # retrieve data
+    data_array = (c_double*3648)()
+    lib.tlccs_getScanData(ccs_handle, byref(data_array))
 
-# plot data
-plt.plot(raman_shift_inverse_cm, intensity)
-plt.xlabel("Wavelength [nm]")
-plt.ylabel("Intensity [a.u.]")
-plt.grid(True)
-plt.show()
+    raman_shift_nm = np.ndarray(shape=(3648,), dtype=float, buffer=wavelengths)
+    raman_shift_inverse_cm = 1e7*(1/532 - 1/raman_shift_nm)
+    intensity = np.ndarray(shape=(3648,), dtype=float, buffer=data_array)
 
-# close
-lib.tlccs_close(ccs_handle)
+    # plot data
+    plt.plot(raman_shift_inverse_cm, intensity)
+    plt.xlabel("Wavelength [cm$^{-1}$]")
+    plt.ylabel("Intensity [a.u.]")
+    plt.grid(True)
+    plt.show()
+
+    # close
+    lib.tlccs_close(ccs_handle)
